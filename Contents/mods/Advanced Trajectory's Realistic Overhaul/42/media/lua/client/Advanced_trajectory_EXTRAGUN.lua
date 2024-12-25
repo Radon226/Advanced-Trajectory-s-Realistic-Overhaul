aa44493db2aba6932a00cb32ac85a7f9a76294b3
Davis <davistran02@gmail.com>
2024-12-24 23:50:33 -0800

    B42 update (unstable)
    - disable throwing (vanilla throw for now)
    - tightened spread for shotguns (big buff)
    - reduced crit chance for shotguns (nerf)
    - added iso cursor for better ease of aiming
    - added bandage to fix inconsistency of shooting zombies stepped on in B42 (hard in b42 for some reason)
    - added support for shooting animals
    - added files to hide vanilla bullet texture
    - fixed issue where bullet spawns if gun is empty
    - hid vanilla gun crosshair
    - fixed error when shooting
    - fixed texture of projectile not spawning
    - fixed weapon attachment nil error
    - added support for new height limits
    - made change so that bullet ignores car if player shooting from a higher z level

---
+++ Contents/mods/Advanced Trajectory's Realistic Overhaul/42/media/lua/client/Advanced_trajectory_EXTRAGUN.lua
@@ -0,0 +1,43 @@
+local Advanced_trajectory = require "Advanced_trajectory_core"
+
+-- REFERENCE 
+--[[
+    local projectilePlayerData = 
+    {
+        item = nil,                             --1 item obj
+        square = nil,                           --2 square obj
+        dirVector = {deltX,deltY},              --3 vector
+        bulletPos = {offX, offY, offZ},         --4 offset BULLET POS
+        bulletDir = dirc,                       --5 direction
+        damage = nil,                           --6 damage
+        bulletDist = handWeapon:getMaxRange() , --7 distance
+        winddir = 1,                            --8 ballistic small categories
+        weaponName = "",                        --9 types
+        rotSpeed = 0,                           --10 rotation speed
+        canPenetrate = false,                   --11 whether it can penetrate
+        bulletSpeed = 1.8,                     --12 ballistic speed
+        iscanbigger = 0,                        --13 can be made bigger
+        projectileType = "",                    --14 ballistic name
+        canPassThroughWall = true,              --15 det whether it can pass through the wall
+        size = 1,                               --16 size
+        currDist = 0,                           --17 current distance
+        distanceConst = 0,                      --18 distance constant
+        player = character,                     --19 players
+        playerPos = {offX, offY, offZ},         --20 original offset PLAYER POS
+        count = 0,                              --21 count
+        throwinfo = {},                         --22 thrown object attributes     
+        isCrit = false                                                  
+    }
+]]
+
+
+-- Example of adding a ranged weapon that shoots bullets/pellets.
+local myGun = {}
+
+myGun.bulletPos = {0.75, 0.75, 0.45}
+myGun.weaponName = "revolver"
+myGun.canPenetrate = true
+myGun.bulletSpeed = 16
+myGun.projectileType = "Base.revolversfx"
+
+Advanced_trajectory.FullWeaponTypes["Base.examplegun"] = myGun
